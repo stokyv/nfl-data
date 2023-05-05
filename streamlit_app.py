cc5c8dc171699d971ad7c8e80e3d6956f15c7f3d
Viet <stokyv@users.noreply.github.com>
2023-05-05 21:01:23 +0700

    make some changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-
+import nfl
 
 st.markdown("# Main page 🎈")
 st.sidebar.markdown("# Main page 🎈")
@@ -10,13 +10,12 @@ st.title("NFL DATA ANALYSIS")
 st.write("A simple app to download NFL data and apply data analysis.")
 
 def load_data(year):
-    filepath = f'C:\\Users\\Chill\\nfl\\play_by_play_{year}.csv.gz'
+    filepath = f'data\play_by_play_2022.csv.gz'
     data = pd.read_csv(filepath, compression='gzip', low_memory=False)                
     return data
 
-df = load_data(2020)
+df = load_data(2022)
 # df
-
 
 
 option = st.selectbox(
